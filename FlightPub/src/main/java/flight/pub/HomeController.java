e8d53d8f9eb01323a8448db3ab23f5952a0b4da3
Thom <thomasbandy23@gmail.com>
2023-05-19 20:27:22 +1000

    Working

--- FlightPub/src/main/java/flight/pub/HomeController.java
+++ FlightPub/src/main/java/flight/pub/HomeController.java
@@ -7,43 +7,25 @@ import io.micronaut.http.annotation.Post;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import io.micronaut.core.annotation.Introspected;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Body;
-import io.micronaut.views.ModelAndView;
 import io.micronaut.views.View;
-import jakarta.inject.Inject;
-// import flight.pub.beans.*;
-import jakarta.inject.Singleton;
+import flight.pub.beans.*;
 
 @Controller("/home")
 public class HomeController {
 
-    // private SearchData searchData;
-    // @Inject
-    // public HomeController(SearchData searchData) {
-    // this.searchData = searchData;
-    // }
-
     @Post(value = "/search", consumes = MediaType.APPLICATION_FORM_URLENCODED) // TODO get form to return json
     @View("display")
     public HttpResponse<?> processSearch(@Body SearchData searchData) {
-        // this.searchData = newSearchData;
+
         System.out.println("WORKING");
         System.out.println(searchData.getLocation() + searchData.getDestination() + searchData.getEndDate()
                 + searchData.getStartDate());
 
         return HttpResponse.ok().body(searchData);
-        // return new ModelAndView<>("display", searchData);
     }
-
-    // @Post(value = "/search", consumes = MediaType.APPLICATION_FORM_URLENCODED)
-    // @View("") // TODO correct
-    // public HttpResponse<?> processDestination(@Body FormData formData) {
-    // System.out.println("WORKING");
-    // return HttpResponse.ok().body(formData);
-    // }
 
     @Get("/")
     @View("index") // TODO correct
@@ -65,60 +47,4 @@ public class HomeController {
 
     // TODO write data object for destination
 
-    @Singleton
-    @Introspected
-    public static class SearchData {
-        // @JsonProperty("location")
-        private String location;
-        // @JsonProperty("destination")
-        private String destination;
-        // @JsonProperty("startDate")
-        private LocalDate startDate;
-        // @JsonProperty("endDate")
-        private LocalDate endDate;
-
-        public String getLocation() {
-
-            return location;
-        }
-
-        public void setLocation(String location) {
-
-            this.location = location;
-        }
-
-        public String getDestination() {
-            return destination;
-        }
-
-        public void setDestination(String destination) {
-            this.destination = destination;
-        }
-
-        public String getStartDate() {
-            if (endDate != null) {
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-                return startDate.format(formatter);
-            } else {
-                return "";
-            }
-        }
-
-        public void setStartDate(LocalDate startDate) {
-            this.startDate = startDate;
-        }
-
-        public String getEndDate() {
-            if (endDate != null) {
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-                return endDate.format(formatter);
-            } else {
-                return "";
-            }
-        }
-
-        public void setEndDate(LocalDate endDate) {
-            this.endDate = endDate;
-        }
-    }
 }
--- FlightPub/src/main/resources/views/display.ftl
+++ FlightPub/src/main/resources/views/display.ftl
@@ -5,9 +5,9 @@
   </head>
   <body>
     <h1>Search Data</h1>
-    <p>Location: ${searchData.location}</p>
-    <p>Destination: ${searchData.destination}</p>
-    <p>Start Date: ${searchData.startDate}</p>
-    <p>End Date: ${searchData.endDate}</p>
+    <p>Location: ${location}</p>
+    <p>Destination: ${destination}</p>
+    <p>Start Date: ${startDate}</p>
+    <p>End Date: ${endDate}</p>
   </body>
 </html>
