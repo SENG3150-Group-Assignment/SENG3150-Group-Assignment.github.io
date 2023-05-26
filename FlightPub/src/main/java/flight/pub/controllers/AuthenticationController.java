6c061c2a8875387a4fe60237c07ba9278efd3551
Thom <thomasbandy23@gmail.com>
2023-05-26 12:11:49 +1000

    Removed unused imports

--- FlightPub/src/main/java/flight/pub/controllers/AuthenticationController.java
+++ FlightPub/src/main/java/flight/pub/controllers/AuthenticationController.java
@@ -7,24 +7,14 @@ import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.views.View;
-import freemarker.core.ParseException;
-import freemarker.template.Configuration;
-import freemarker.template.MalformedTemplateNameException;
-import freemarker.template.Template;
-import freemarker.template.TemplateException;
-import freemarker.template.TemplateNotFoundException;
 
-import java.io.IOException;
-import java.io.StringWriter;
+
+
 import java.net.URI;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
-import flight.pub.beans.DestinationBean;
-import flight.pub.beans.SearchBean;
+
 import flight.pub.beans.UserBean;
-import flight.pub.controllers.*;
+
 
 @Controller("/auth")
 public class AuthenticationController {
